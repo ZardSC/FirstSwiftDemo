aa7a65302fd743f230dad05ed62000d9b0bbcfc9
jxsWinnie <jxs_winnie@126.com>
2016-02-11 12:31:46 +0800

    Initial Commit

---
+++ FirstDemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstDemoProject
+//
+//  Created by Winnie on 16/2/11.
+//  Copyright © 2016年 Winnie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
